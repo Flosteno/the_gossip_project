e38589084318621f17b01632c68c6d8fa412c9a3
flosteno <florian-benoit@outlook.fr>
2025-06-10 12:03:57 +0200

    Ajout des Validations

--- app/models/private_message.rb
+++ app/models/private_message.rb
@@ -2,4 +2,6 @@ class PrivateMessage < ApplicationRecord
   belongs_to :sender, class_name: 'User'
   has_many :recipients
   has_many :users, through: :recipients
+  validates :content, presence: true
+  validates :sender_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
   has_many :sent_messages, class_name: 'PrivateMessage', foreign_key: 'sender_id'
   has_many :recipients
   has_many :received_messages, through: :recipients, source: :private_message
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :description, presence: true
+  validates :email, presence: true
+  validates :age, presence: true
+  validates :city, presence: true
 end
